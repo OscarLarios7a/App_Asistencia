e182b8ccd8552e8077483e29ac4b746872acb0fb
Oscar Larios <javier.atoaxaca@gmail.com>
2021-04-19 20:48:49 -0500

    se creo el archivo LPersonal.cs en la capa de Logica, para cargar y obtener los datos de dicha clase a traves de la capa de presentacion.

---
+++ AppAsistencia/Logica/LPersonal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAsistencia.Logica
+{
+    //se Genera la clase de LPersonal para evitar errores al llamarla desde otra capa por ello se le pone "public class"
+    public class LPersonal
+   {
+        //        [Id_personal]
+        //        [int] IDENTITY(1,1) NOT NULL,
+
+        //[Nombres] [varchar](max) NULL,
+        //	[Identificacion]
+        //        [varchar](max) NULL,
+        //	[Pais]
+        //        [varchar](max) NULL,
+        //	[Id_cargo]
+        //        [int] NULL,
+        //	[SueldoPorHora]
+        //        [numeric](18, 2) NULL,
+        //	[Estado]
+        //        [varchar](max) NULL,
+        //	[codigo]
+        //        [varchar](max) NULL,
+        public int Id_personal { get; set; }
+        public string Nombres { get; set; }
+
+
+    }
+}
